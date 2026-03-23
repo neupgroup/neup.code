9616b7e73d6ad5971b34b986f927ad254ed9bc11
Kishor Neupane <114856857+neupkishor@users.noreply.github.com>
2026-03-23 22:40:10 +0545

    Updated the prisma location.

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,7 +1,7 @@
 import "server-only";
 
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "@/generated/prisma/client";
+import { PrismaClient } from "@/prisma/generated/client";
 
 function createPrismaClient() {
   const connectionString = process.env.DATABASE_URL;
